ed5c81066663bf80f4f1fa5e0743aefc84d8a5c7
bcolloran <bcolloran@mozilla.com>
2013-09-19 13:38:14 -0700

    syncing repositories with jydoopSync.sh 2013-09-19.13.38

--- scripts/fennecQuery_searchByGeo.py
+++ scripts/fennecQuery_searchByGeo.py
@@ -14,26 +14,26 @@ make ARGS="scripts/fennecQuery_searchByGeo.py /outData/fennec_searchByGeo.csv /d
 
 '''
 
-# def setupjob(job, args):
-#     """
-#     Set up a job to run on one or more HDFS locations
+def setupjob(job, args):
+    """
+    Set up a job to run on one or more HDFS locations
 
-#     Jobs expect one or more arguments, the HDFS path(s) to the data.
-#     """
+    Jobs expect one or more arguments, the HDFS path(s) to the data.
+    """
     
-#     import org.apache.hadoop.mapreduce.lib.input.FileInputFormat as FileInputFormat
-#     import org.apache.hadoop.mapreduce.lib.input.SequenceFileAsTextInputFormat as MyInputFormat
+    import org.apache.hadoop.mapreduce.lib.input.FileInputFormat as FileInputFormat
+    import org.apache.hadoop.mapreduce.lib.input.SequenceFileAsTextInputFormat as MyInputFormat
 
-#     if len(args) < 1:
-#         raise Exception("Usage: <hdfs-location1> [ <location2> ] [ <location3> ] [ ... ]")
+    if len(args) < 1:
+        raise Exception("Usage: <hdfs-location1> [ <location2> ] [ <location3> ] [ ... ]")
 
-#     job.setInputFormatClass(MyInputFormat)
-#     FileInputFormat.setInputPaths(job, ",".join(args));
-#     job.getConfiguration().set("org.mozilla.jydoop.mappertype", "TEXT")
-#     # set the job to run in the RESEARCH queue
-#     job.getConfiguration().set("mapred.job.queue.name","research")
+    job.setInputFormatClass(MyInputFormat)
+    FileInputFormat.setInputPaths(job, ",".join(args));
+    job.getConfiguration().set("org.mozilla.jydoop.mappertype", "TEXT")
+    # set the job to run in the RESEARCH queue
+    job.getConfiguration().set("mapred.job.queue.name","research")
 
-setupjob=healthreportutils_v3.setupjob
+# setupjob=healthreportutils_v3.setupjob
 
 
 @healthreportutils_v3.FHRMapper()
