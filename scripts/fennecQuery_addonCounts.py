8805b8047a341685db338620b2ce82cfc6a29ef5
bcolloran <bcolloran@mozilla.com>
2013-09-27 16:10:29 -0700

    syncing repositories with jydoopSync.sh 2013-09-27.16.10

--- scripts/fennecQuery_addonCounts.py
+++ scripts/fennecQuery_addonCounts.py
@@ -16,24 +16,25 @@ make ARGS="scripts/fennecQuery_searchByGeo.py /outData/fennec_searchByGeo.csv /d
 make ARGS="scripts/fennecQuery_searchByGeo.py /outData/fennec_searchByGeo.csv /user/sguha/fhr/samples/output/1pct" hadoop
 
 '''
+setupjob = healthreportutils_v3.setupjob
 
-def setupjob(job, args):
-    """
-    Set up a job to run on one or more HDFS locations
+# def setupjob(job, args):
+#     """
+#     Set up a job to run on one or more HDFS locations
 
-    Jobs expect one or more arguments, the HDFS path(s) to the data.
-    """
-    import org.apache.hadoop.mapreduce.lib.input.FileInputFormat as FileInputFormat
-    import org.apache.hadoop.mapreduce.lib.input.SequenceFileAsTextInputFormat as MyInputFormat
+#     Jobs expect one or more arguments, the HDFS path(s) to the data.
+#     """
+#     import org.apache.hadoop.mapreduce.lib.input.FileInputFormat as FileInputFormat
+#     import org.apache.hadoop.mapreduce.lib.input.SequenceFileAsTextInputFormat as MyInputFormat
 
-    if len(args) < 1:
-        raise Exception("Usage: <hdfs-location1> [ <location2> ] [ <location3> ] [ ... ]")
+#     if len(args) < 1:
+#         raise Exception("Usage: <hdfs-location1> [ <location2> ] [ <location3> ] [ ... ]")
 
-    job.setInputFormatClass(MyInputFormat)
-    FileInputFormat.setInputPaths(job, ",".join(args));
-    job.getConfiguration().set("org.mozilla.jydoop.mappertype", "TEXT")
-    # set the job to run in the RESEARCH queue
-    job.getConfiguration().set("mapred.job.queue.name","research")
+#     job.setInputFormatClass(MyInputFormat)
+#     FileInputFormat.setInputPaths(job, ",".join(args));
+#     job.getConfiguration().set("org.mozilla.jydoop.mappertype", "TEXT")
+#     # set the job to run in the RESEARCH queue
+#     job.getConfiguration().set("mapred.job.queue.name","research")
 
 
 @healthreportutils_v3.FHRMapper()
