8a2792eccc11f0cd101973e04951b6edbf66df20
bcolloran <bcolloran@mozilla.com>
2013-10-25 13:00:51 -0700

    syncing repositories with jydoopSync.sh 2013-10-25.13.00

--- scripts/orphaningDatesFennec_extraInfo_process.py
+++ scripts/orphaningDatesFennec_extraInfo_process.py
@@ -8,8 +8,7 @@ import random
 in following commands, UPDATE DATES
 
 ----to run against HDFS sample
-make ARGS="scripts/orphaningDatesFennec_extraInfo_process.py ./outData/orphaningDatesFennec_extraInfo_processed.csv /user/bcolloran/outData/orphaningDatesFennec_extraInfo_2013-10-24/part*
-" hadoop
+make ARGS="scripts/orphaningDatesFennec_extraInfo_process.py ./outData/orphaningDatesFennec_extraInfo_processed.csv /user/bcolloran/outData/orphaningDatesFennec_extraInfo_2013-10-24/part*" hadoop
 
 '''
 
@@ -42,7 +41,7 @@ def map(key, value, context):
 
     #otherwise, the key will be a (firstDay,hash(firstDayDataStr)) tuple fingerprint, and the value will be a list of thisPingDates for each record associated with the fingerprint. there should be at least two thisPingDates
     # emit a "1" for each thisPingDate
-    for thisPingDate in sorted(value)[:-1]:
+    for thisPingDate in sorted(list(value))[:-1]:
         context.write(thisPingDate,1)
 
 
