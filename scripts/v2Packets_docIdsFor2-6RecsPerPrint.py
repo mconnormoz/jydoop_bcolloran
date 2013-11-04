42df9742a0c5c7f2a9112f253d1fce67bb29cefa
bcolloran <bcolloran@mozilla.com>
2013-11-04 15:28:45 -0800

    syncing repositories with jydoopSync.sh 2013-11-04.15.28

--- scripts/v2Packets_docIdsFor2-6RecsPerPrint.py
+++ scripts/v2Packets_docIdsFor2-6RecsPerPrint.py
@@ -18,25 +18,7 @@ make ARGS="scripts/v2Packets_docIdsFor2-6RecsPerPrint.py ./outData/v2Packets_doc
 #     return True
 
 
-# setupjob = healthreportutils.setupjob
-
-def setupjob(job, args):
-    """
-    Set up a job to run on one or more HDFS locations
-
-    Jobs expect one or more arguments, the HDFS path(s) to the data.
-    """
-    import org.apache.hadoop.mapreduce.lib.input.FileInputFormat as FileInputFormat
-    import org.apache.hadoop.mapreduce.lib.input.SequenceFileAsTextInputFormat as MyInputFormat
-
-    if len(args) < 1:
-        raise Exception("Usage: <hdfs-location1> [ <location2> ] [ <location3> ] [ ... ]")
-
-    job.setInputFormatClass(MyInputFormat)
-    FileInputFormat.setInputPaths(job, ",".join(args));
-    job.getConfiguration().set("org.mozilla.jydoop.mappertype", "TEXT")
-    # set the job to run in the RESEARCH queue
-    job.getConfiguration().set("mapred.job.queue.name","research")
+setupjob = healthreportutils.setupjob
 
 
 
