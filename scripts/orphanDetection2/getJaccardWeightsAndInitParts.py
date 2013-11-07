e3584b95faf0522e8a2004eec66604a27a07b25c
bcolloran <bcolloran@mozilla.com>
2013-11-06 16:28:22 -0800

    syncing repositories with jydoopSync.sh 2013-11-06.16.28

--- scripts/orphanDetection2/getJaccardWeightsAndInitParts.py
+++ scripts/orphanDetection2/getJaccardWeightsAndInitParts.py
@@ -26,7 +26,7 @@ def setupjob(job, args):
 
     job.setInputFormatClass(MyInputFormat)
     FileInputFormat.setInputPaths(job, ",".join(args));
-    job.getConfiguration().set("org.mozilla.jydoop.mappertype", "TEXT")
+    job.getConfiguration().set("org.mozilla.jydoop.mappertype", "JYDOOP")
     # set the job to run in the RESEARCH queue
     job.getConfiguration().set("mapred.job.queue.name","research")
 
