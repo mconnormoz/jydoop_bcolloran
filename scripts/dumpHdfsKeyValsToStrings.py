0ee4cede49c7cffd01c7e76bc4e3508c25054acf
bcolloran <bcolloran@mozilla.com>
2013-11-08 11:35:17 -0800

    syncing repositories with jydoopSync.sh 2013-11-08.11.35

--- scripts/dumpHdfsKeyValsToStrings.py
+++ scripts/dumpHdfsKeyValsToStrings.py
@@ -1,34 +1,32 @@
+import jydoop
+
 '''
 in following commands, UPDATE DATES
 
-make ARGS="scripts/orphanDetection2/getJaccardWeightsAndInitParts.py ./outData/weightedEdgesInParts /user/bcolloran/outData/recordsSharingDatePrint/part-r*" hadoop
+make ARGS="scripts/orphanDetection2/dumpHdfsKeyValsToStrings.py ./outData/PATH_ON_PEACH_TO_DUMP_TO ./PATH_ON_HDFS_TO_DUMP" hadoop
 
 '''
 
-######## to OUTPUT TO HDFS from RAW HBASE
-# def skip_local_output():
-#     return True
 
+setupjob = jydoop.setupjob
 
-# setupjob = healthreportutils.setupjob
+# def setupjob(job, args):
+#     """
+#     Set up a job to run on one or more HDFS locations
 
-def setupjob(job, args):
-    """
-    Set up a job to run on one or more HDFS locations
+#     Jobs expect one or more arguments, the HDFS path(s) to the data.
+#     """
+#     import org.apache.hadoop.mapreduce.lib.input.FileInputFormat as FileInputFormat
+#     import org.apache.hadoop.mapreduce.lib.input.SequenceFileAsTextInputFormat as MyInputFormat
 
-    Jobs expect one or more arguments, the HDFS path(s) to the data.
-    """
-    import org.apache.hadoop.mapreduce.lib.input.FileInputFormat as FileInputFormat
-    import org.apache.hadoop.mapreduce.lib.input.SequenceFileAsTextInputFormat as MyInputFormat
+#     if len(args) < 1:
+#         raise Exception("Usage: <hdfs-location1> [ <location2> ] [ <location3> ] [ ... ]")
 
-    if len(args) < 1:
-        raise Exception("Usage: <hdfs-location1> [ <location2> ] [ <location3> ] [ ... ]")
-
-    job.setInputFormatClass(MyInputFormat)
-    FileInputFormat.setInputPaths(job, ",".join(args));
-    job.getConfiguration().set("org.mozilla.jydoop.mappertype", "TEXT")
-    # set the job to run in the RESEARCH queue
-    job.getConfiguration().set("mapred.job.queue.name","research")
+#     job.setInputFormatClass(MyInputFormat)
+#     FileInputFormat.setInputPaths(job, ",".join(args));
+#     job.getConfiguration().set("org.mozilla.jydoop.mappertype", "TEXT")
+#     # set the job to run in the RESEARCH queue
+#     job.getConfiguration().set("mapred.job.queue.name","research")
 
 
 
