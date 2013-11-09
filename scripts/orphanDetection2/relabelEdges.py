bbe6df66e7ec53bc17c9c58895b912ee10a90ad1
bcolloran <bcolloran@mozilla.com>
2013-11-08 17:21:58 -0800

    syncing repositories with jydoopSync.sh 2013-11-08.17.21

--- scripts/orphanDetection2/relabelEdges.py
+++ scripts/orphanDetection2/relabelEdges.py
@@ -8,8 +8,8 @@ make ARGS="scripts/orphanDetection2/relabelEdges.py ./outData/relabeledEdges ./o
 '''
 
 ######## to OUTPUT TO HDFS from RAW HBASE
-# def skip_local_output():
-#     return True
+def skip_local_output():
+    return True
 
 
 setupjob = jydoop.setupjob
