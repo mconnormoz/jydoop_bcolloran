bf8ba4e167e1504a27626d2b6d1781df9588b43b
bcolloran <bcolloran@mozilla.com>
2014-02-07 18:33:35 -0800

    syncing repositories with jydoopSync.sh 2014-02-07.18.33

--- scripts/hdfsTools/dumpHdfsKeyValsToStrings.py
+++ scripts/hdfsTools/dumpHdfsKeyValsToStrings.py
@@ -1,42 +1,15 @@
 import jydoop
+import orphUtils
+
 
 '''
 in following commands, UPDATE DATES
 
 make ARGS="scripts/dumpHdfsKeyValsToStrings.py ./outData/PATH_ON_PEACH_TO_DUMP_TO ./PATH_ON_HDFS_TO_DUMP" hadoop
-
-/user/bcolloran/outData/orphIterTest/edgeWeightsAndInitParts
-/user/bcolloran/outData/orphIterTest/partsOverlap2
-/user/bcolloran/outData/orphIterTest/partsOverlap3
-/user/bcolloran/outData/orphIterTest/relabeledEdges1
-
-
-make ARGS="scripts/dumpHdfsKeyValsToStrings.py ./outData/edgeWeightsAndInitParts ./outData/orphIterTest/edgeWeightsAndInitParts" hadoop
-
-make ARGS="scripts/dumpHdfsKeyValsToStrings.py ./outData/partsOverlap2 ./outData/orphIterTest/partsOverlap2" hadoop
-
 '''
 
+setupjob = orphUtils.hdfsjobByType("JYDOOP")
 
-# setupjob = jydoop.setupjob
-
-
-def setupjob(job, args):
-    """
-    Set up a job to run on a list of paths.  Jobs expect at least one path,
-    but you may specify as many as you like.
-    """
-
-    import org.apache.hadoop.mapreduce.lib.input.FileInputFormat as FileInputFormat
-    import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat as MyInputFormat
-
-    if len(args) < 1:
-        raise Exception("Usage: path [ path2 ] [ path3 ] [ ... ]")
-
-    job.setInputFormatClass(MyInputFormat)
-    FileInputFormat.setInputPaths(job, ",".join(args));
-    """Indicate to HadoopDriver which Mapper we want to use."""
-    job.getConfiguration().set("org.mozilla.jydoop.mappertype", "JYDOOP")
 
 def output(path, results):
     # just dump tab separated key/vals
