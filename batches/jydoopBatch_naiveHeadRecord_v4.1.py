2ba4fa8d8ccfb0c20c759d9c788a3f88a2a8d51b
bcolloran <bcolloran@mozilla.com>
2014-03-14 16:59:55 -0700

    syncing repositories with jydoopSync.sh 2014-03-14.16.59

--- batches/jydoopBatch_naiveHeadRecord_v4.1.py
+++ batches/jydoopBatch_naiveHeadRecord_v4.1.py
@@ -6,40 +6,40 @@ import subprocess
 
 
 
-# extractDate=datetime.datetime.utcnow().isoformat()[0:10]
+extractDate=datetime.datetime.utcnow().isoformat()[0:10]
 
 
 
-# if socket.gethostname()=='peach-gw.peach.metrics.scl3.mozilla.com':
-#     print "================ PEACH RUN ================"
+if socket.gethostname()=='peach-gw.peach.metrics.scl3.mozilla.com':
+    print "================ PEACH RUN ================"
  
-#     batchEnv = jydoopBatch.env(jydoopRoot="/home/bcolloran/jydoop_bcolloran2/jydoop/",
-#         scriptRoot="scripts/orphanDetection4.1/",
-#         dataRoot="/user/bcolloran/data/fhrDeorphaning_"+extractDate+"/",
-#         logPath="outData/orphIterLogs4.1/",
-#         verbose=True,
-#         onCluster=True,
-#         batchName="naive head record extraction")
-#         #HDFS paths
-#     initInDataPath = "/user/bcolloran/data/fhrFullExtract_"+extractDate
-#     #"/tmp/full_dumb_export"
-#     # "/tmp/full_dumb_export/part-m-*01"
-#     # "/user/bcolloran/data/samples/fhr/v2/withOrphans/2013-11-05/part-r-0001*"
-#     # "/user/bcolloran/data/samples/fhr/v2/withOrphans/2013-11-05/"
-#     # 
-# else:
-#     print "================ LOCAL RUN ================"
-#     rootPath = "/data/mozilla/jydoop_bcolloran/"
-#     batchEnv = jydoopBatch.env(jydoopRoot=rootPath,
-#         scriptRoot="scripts/orphanDetection4.1/",
-#         dataRoot=rootPath+"testData/orph4.1/",
-#         logPath="testData/orph4.0/",
-#         verbose=True,
-#         onCluster=False,
-#         batchName="naive head record extraction")
+    batchEnv = jydoopBatch.env(jydoopRoot="/home/bcolloran/jydoop_bcolloran2/jydoop/",
+        scriptRoot="scripts/orphanDetection4.1/",
+        dataRoot="/user/bcolloran/data/fhrDeorphaning_"+extractDate+"/",
+        logPath="outData/orphIterLogs4.1/",
+        verbose=True,
+        onCluster=True,
+        batchName="naive head record extraction")
+        #HDFS paths
+    initInDataPath = "/user/bcolloran/data/fhrFullExtract_"+extractDate
+    #"/tmp/full_dumb_export"
+    # "/tmp/full_dumb_export/part-m-*01"
+    # "/user/bcolloran/data/samples/fhr/v2/withOrphans/2013-11-05/part-r-0001*"
+    # "/user/bcolloran/data/samples/fhr/v2/withOrphans/2013-11-05/"
+    # 
+else:
+    print "================ LOCAL RUN ================"
+    rootPath = "/data/mozilla/jydoop_bcolloran/"
+    batchEnv = jydoopBatch.env(jydoopRoot=rootPath,
+        scriptRoot="scripts/orphanDetection4.1/",
+        dataRoot=rootPath+"testData/orph4.1/",
+        logPath="testData/orph4.0/",
+        verbose=True,
+        onCluster=False,
+        batchName="naive head record extraction")
 
-#     initInDataPath = "/data/mozilla/jydoop_bcolloran/testData/sampleOfRecordsWithOrphans_2013-11-05_1000rec.txt"
-#     # initInDataPath = "/home/bcolloran/Desktop/projects/jydoop_bcolloran/testData/sampleOfFhrPacketsWithDuplicatedFingerprint_afterMultiDelete_2013-08-05.jydoopRaw"
+    initInDataPath = "/data/mozilla/jydoop_bcolloran/testData/sampleOfRecordsWithOrphans_2013-11-05_1000rec.txt"
+    # initInDataPath = "/home/bcolloran/Desktop/projects/jydoop_bcolloran/testData/sampleOfFhrPacketsWithDuplicatedFingerprint_afterMultiDelete_2013-08-05.jydoopRaw"
 
 
 
