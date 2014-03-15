027027868926143cf563b761e208dcc16bc834c5
bcolloran <bcolloran@mozilla.com>
2014-03-15 10:46:26 -0700

    syncing repositories with jydoopSync.sh 2014-03-15.10.46

--- batches/jydoopBatch_naiveHeadRecord_v4.1.py
+++ batches/jydoopBatch_naiveHeadRecord_v4.1.py
@@ -45,158 +45,157 @@ else:
 
 
 
-# '''
-# to make a snapshot on peach:
-# cd /home/bcolloran/pig
-# pig -param OUTPUT=/user/bcolloran/data/fhrFullExtract_%s hbase_export.pig -D pig.additional.jars=./elephant-bird-core-4.3.jar:./elephant-bird-hadoop-compat-4.3.jar:./elephant-bird-pig-4.3.jar
-# '''
-# # if batchEnv.onCluster:
-# #     os.chdir("/home/bcolloran/pig/")
-# #     commandList = ["pig","-param","OUTPUT=/user/bcolloran/data/fhrFullExtract_%s"%extractDate,"hbase_export.pig","-D","pig.additional.jars=./elephant-bird-core-4.3.jar:./elephant-bird-hadoop-compat-4.3.jar:./elephant-bird-pig-4.3.jar"]
-# #     command = "pig -param OUTPUT=/user/bcolloran/data/fhrFullExtract_%s hbase_export.pig -D pig.additional.jars=./elephant-bird-core-4.3.jar:./elephant-bird-hadoop-compat-4.3.jar:./elephant-bird-pig-4.3.jar"%extractDate
-# #     jydoopBatch.runCommand(batchEnv,command)
-# #     os.chdir("/home/bcolloran/jydoop_bcolloran2/jydoop/")
-
-
+'''
+to make a snapshot on peach:
+cd /home/bcolloran/pig
+pig -param OUTPUT=/user/bcolloran/data/fhrFullExtract_%s hbase_export.pig -D pig.additional.jars=./elephant-bird-core-4.3.jar:./elephant-bird-hadoop-compat-4.3.jar:./elephant-bird-pig-4.3.jar
+'''
 # if batchEnv.onCluster:
 #     os.chdir("/home/bcolloran/pig/")
+#     commandList = ["pig","-param","OUTPUT=/user/bcolloran/data/fhrFullExtract_%s"%extractDate,"hbase_export.pig","-D","pig.additional.jars=./elephant-bird-core-4.3.jar:./elephant-bird-hadoop-compat-4.3.jar:./elephant-bird-pig-4.3.jar"]
 #     command = "pig -param OUTPUT=/user/bcolloran/data/fhrFullExtract_%s hbase_export.pig -D pig.additional.jars=./elephant-bird-core-4.3.jar:./elephant-bird-hadoop-compat-4.3.jar:./elephant-bird-pig-4.3.jar"%extractDate
-#     p=subprocess.call(command,shell=True)
+#     jydoopBatch.runCommand(batchEnv,command)
 #     os.chdir("/home/bcolloran/jydoop_bcolloran2/jydoop/")
 
 
+if batchEnv.onCluster:
+    os.chdir("/home/bcolloran/pig/")
+    command = "pig -param OUTPUT=/user/bcolloran/data/fhrFullExtract_%s hbase_export.pig -D pig.additional.jars=./elephant-bird-core-4.3.jar:./elephant-bird-hadoop-compat-4.3.jar:./elephant-bird-pig-4.3.jar"%extractDate
+    p=subprocess.call(command,shell=True)
+    os.chdir("/home/bcolloran/jydoop_bcolloran2/jydoop/")
 
 
 
 
-# print "\n==== initialize graph parts"
-# jydoopBatch.job(batchEnv,
-#     "initRecordScan_kDocId_vPartOrTieBreakInfo.py",
-#     initInDataPath,
-#     "kDocId_vPartOrTieBreakInfo").run()
 
 
-# print "\n==== find initial part overlaps, skip tieBreakInfo"
-# numOverlapping = jydoopBatch.job(batchEnv,
-#         "findTouchingDocsAndParts.py",
-#         "kDocId_vPartOrTieBreakInfo",
-#         "kPart_vObjTouchingPart_0")\
-#     .run().getCounterVal("OVERLAPPING_PARTS")
-
-# print "==initial number overlapping:",numOverlapping
+print "\n==== initialize graph parts"
+jydoopBatch.job(batchEnv,
+    "initRecordScan_kDocId_vPartOrTieBreakInfo.py",
+    initInDataPath,
+    "kDocId_vPartOrTieBreakInfo").run()
 
 
+print "\n==== find initial part overlaps, skip tieBreakInfo"
+numOverlapping = jydoopBatch.job(batchEnv,
+        "findTouchingDocsAndParts.py",
+        "kDocId_vPartOrTieBreakInfo",
+        "kPart_vObjTouchingPart_0")\
+    .run().getCounterVal("OVERLAPPING_PARTS")
 
-# graphIter = 0
-# print "\n================ iteration ================",graphIter
-# while graphIter<10:
+print "==initial number overlapping:",numOverlapping
+
+
+
+graphIter = 0
+print "\n================ iteration ================",graphIter
+while graphIter<10:
     
-#     if numOverlapping==0:
-#         convergedFlag=True
-#         break
+    if numOverlapping==0:
+        convergedFlag=True
+        break
 
-#     jydoopBatch.job(batchEnv,
-#         script="relabelDocsWithLowestPart.py",
-#         inPathList="kPart_vObjTouchingPart_"+str(graphIter),
-#         outPath="kDoc_vPart_"+str(graphIter+1)).run()
-#     graphIter+=1
+    jydoopBatch.job(batchEnv,
+        script="relabelDocsWithLowestPart.py",
+        inPathList="kPart_vObjTouchingPart_"+str(graphIter),
+        outPath="kDoc_vPart_"+str(graphIter+1)).run()
+    graphIter+=1
 
-#     print "\n==== check for overlaps",graphIter
-#     numOverlapping = jydoopBatch.job(batchEnv,
-#         "findTouchingDocsAndParts.py",
-#         "kDoc_vPart_"+str(graphIter),
-#         "kPart_vObjTouchingPart_"+str(graphIter+1))\
-#     .run().getCounterVal("OVERLAPPING_PARTS")
-#     graphIter+=1
-#     print "==number overlapping:",numOverlapping
-
-
-
-# if convergedFlag:
-#     print "\n================ graph converged ================ iter:",graphIter,"\n"
-#     batchEnv.log("\n================ graph converged ================ iter: "+str(graphIter)+"\n")
-# else:
-#     print "\n====== graph FAILED TO converge on iter:",graphIter
-#     print "(some kind of error occurred)\n"
-#     exit()
+    print "\n==== check for overlaps",graphIter
+    numOverlapping = jydoopBatch.job(batchEnv,
+        "findTouchingDocsAndParts.py",
+        "kDoc_vPart_"+str(graphIter),
+        "kPart_vObjTouchingPart_"+str(graphIter+1))\
+    .run().getCounterVal("OVERLAPPING_PARTS")
+    graphIter+=1
+    print "==number overlapping:",numOverlapping
 
 
 
-
-# '''
-# now that the graph has converged, we have:
-# kDocId_vPartOrTieBreakInfo
-# kPart_vObjTouchingPart_${finalIter}
-
-# we want final_kNaiveHeadRecordDocId_vPart
-
-# to get there,
-# final_kNaiveHeadRecordDocId_vPart needs INPUT:
-#     kPartId_vDocId-tieBreakInfo
-# with vDocId-tieBreakInfo like: ("docId",(thisPingDate, numAppSessionsPreviousOnThisPingDate, currentSessionTime))
-
-# to get kPartId_vDocId-tieBreakInfo, need to join 
-#     kDocId_vPartOrTieBreakInfo
-# with
-#     kPart_vObjTouchingPart_${finalIter}
-# '''
+if convergedFlag:
+    print "\n================ graph converged ================ iter:",graphIter,"\n"
+    batchEnv.log("\n================ graph converged ================ iter: "+str(graphIter)+"\n")
+else:
+    print "\n====== graph FAILED TO converge on iter:",graphIter
+    print "(some kind of error occurred)\n"
+    exit()
 
 
 
 
+'''
+now that the graph has converged, we have:
+kDocId_vPartOrTieBreakInfo
+kPart_vObjTouchingPart_${finalIter}
 
+we want final_kNaiveHeadRecordDocId_vPart
 
-# # graphIter=2 ###################### WARNING!!!!
+to get there,
+final_kNaiveHeadRecordDocId_vPart needs INPUT:
+    kPartId_vDocId-tieBreakInfo
+with vDocId-tieBreakInfo like: ("docId",(thisPingDate, numAppSessionsPreviousOnThisPingDate, currentSessionTime))
 
-
-
-
-# print "==== join kDocId_vPartOrTieBreakInfo with kPart_vObjTouchingPart_${finalIter}"
-# jydoopBatch.job(batchEnv,
-#     "join_kDocIdVPartId_toTieBreakinfo.py",
-#     ["kDocId_vPartOrTieBreakInfo","kPart_vObjTouchingPart_"+str(graphIter)],
-#     "kPartId_vDocId-tieBreakInfo").run()
-
-
-
-
-# '''
-# next take the tie breaker info for each part, and emit the head doc id for that part
-
-#     input must be:
-#       k: "partId"
-#       v: ("docId",(thisPingDate, numAppSessionsPreviousOnThisPingDate, currentSessionTime))
-# '''
-
-
-
-
-# if batchEnv.onCluster:
-#     """this has to be run "by hand" when on peach, because jydoop requires relative path names to save locally (to peach-gw).
-# ex:
-# make ARGS="scripts/orphanDetection4.1/final_kNaiveHeadRecordDocId_vPartId.py outData/orphanDetection4/fullExport_2014-02-27_final_naiveHeadRecordDocId.txt /user/bcolloran/data/fhrDeorphaning_2014-02-27/kPartId_vDocId-tieBreakInfo" hadoop"""
-#     os.chdir("/home/bcolloran/jydoop_bcolloran2/jydoop/")
-#     command = 'make ARGS="scripts/orphanDetection4.1/final_kNaiveHeadRecordDocId_vPartId.py outData/orphanDetection4/fullExport_%s_final_naiveHeadRecordDocId.txt /user/bcolloran/data/fhrDeorphaning_%s/kPartId_vDocId-tieBreakInfo" hadoop'%(extractDate,extractDate)
-#     p=subprocess.call(command,shell=True)
-
-#     # jydoopBatch.runCommand(batchEnv,
-#     #     ["make",
-#     #     'ARGS="scripts/orphanDetection4.1/final_kNaiveHeadRecordDocId_vPartId.py outData/orphanDetection4/fullExport_%s_final_naiveHeadRecordDocId.txt /user/bcolloran/data/fhrDeorphaning_%s/kPartId_vDocId-tieBreakInfo"'%(extractDate,extractDate),
-#     #     "hadoop"])
-# else:
-#     '''if running locally, the absolute'''
-#     print "\n==== get naive head doc id for each part"
-#     jydoopBatch.job(batchEnv,
-#         "final_kNaiveHeadRecordDocId_vPartId.py",
-#         "kPartId_vDocId-tieBreakInfo",
-#         "final_naiveHeadRecordDocId.txt").run()
+to get kPartId_vDocId-tieBreakInfo, need to join 
+    kDocId_vPartOrTieBreakInfo
+with
+    kPart_vObjTouchingPart_${finalIter}
+'''
 
 
 
 
 
 
-extractDate="2014-03-11"
+# graphIter=2 ###################### WARNING!!!!
+
+
+
+
+print "==== join kDocId_vPartOrTieBreakInfo with kPart_vObjTouchingPart_${finalIter}"
+jydoopBatch.job(batchEnv,
+    "join_kDocIdVPartId_toTieBreakinfo.py",
+    ["kDocId_vPartOrTieBreakInfo","kPart_vObjTouchingPart_"+str(graphIter)],
+    "kPartId_vDocId-tieBreakInfo").run()
+
+
+
+
+'''
+next take the tie breaker info for each part, and emit the head doc id for that part
+
+    input must be:
+      k: "partId"
+      v: ("docId",(thisPingDate, numAppSessionsPreviousOnThisPingDate, currentSessionTime))
+'''
+
+
+
+
+if batchEnv.onCluster:
+    """this has to be run "by hand" when on peach, because jydoop requires relative path names to save locally (to peach-gw).
+ex:
+make ARGS="scripts/orphanDetection4.1/final_kNaiveHeadRecordDocId_vPartId.py outData/orphanDetection4/fullExport_2014-02-27_final_naiveHeadRecordDocId.txt /user/bcolloran/data/fhrDeorphaning_2014-02-27/kPartId_vDocId-tieBreakInfo" hadoop"""
+    os.chdir("/home/bcolloran/jydoop_bcolloran2/jydoop/")
+    command = 'make ARGS="scripts/orphanDetection4.1/final_kNaiveHeadRecordDocId_vPartId.py outData/orphanDetection4/fullExport_%s_final_naiveHeadRecordDocId.txt /user/bcolloran/data/fhrDeorphaning_%s/kPartId_vDocId-tieBreakInfo" hadoop'%(extractDate,extractDate)
+    p=subprocess.call(command,shell=True)
+
+    # jydoopBatch.runCommand(batchEnv,
+    #     ["make",
+    #     'ARGS="scripts/orphanDetection4.1/final_kNaiveHeadRecordDocId_vPartId.py outData/orphanDetection4/fullExport_%s_final_naiveHeadRecordDocId.txt /user/bcolloran/data/fhrDeorphaning_%s/kPartId_vDocId-tieBreakInfo"'%(extractDate,extractDate),
+    #     "hadoop"])
+else:
+    '''if running locally, the absolute'''
+    print "\n==== get naive head doc id for each part"
+    jydoopBatch.job(batchEnv,
+        "final_kNaiveHeadRecordDocId_vPartId.py",
+        "kPartId_vDocId-tieBreakInfo",
+        "final_naiveHeadRecordDocId.txt").run()
+
+
+
+
+
+
 
 
 if batchEnv.onCluster:
@@ -212,6 +211,7 @@ pig -param orig=/user/bcolloran/data/fhrFullExtract_2014-02-27/ -param fetchids=
     # p=subprocess.call(command,shell=True)
     os.chdir("/home/bcolloran/pig/")
     command = 'pig -param orig=/user/bcolloran/data/fhrFullExtract_%s/ -param fetchids=/user/bcolloran/data/fhrDeorphaning_%s/headRecordsFinalDocIds.txt -param jointype=merge -param output=fhrDeorphaned_%s fetch_reports.aphadke.pig'%(extractDate,extractDate,extractDate)
+    os.chdir("/home/bcolloran/jydoop_bcolloran2/jydoop/")
     p=subprocess.call(command,shell=True)
 
 
